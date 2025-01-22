368a0b5e2fdb6f14e87ec58def4d69ca54102ac9
Shreyas-S-smarty <shreyassrsr@gmail.com>
2025-01-22 14:36:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+
+app.post("/user",(req,res)=>{
+ res.json({success:"true"})
+
+})
+
+
+
+app.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
 
